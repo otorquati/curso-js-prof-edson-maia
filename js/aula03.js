79cf52c6c8967db3ac11a30f6a9c627f73b26ddb
otorquati <torquati.osvaldo@gmail.com>
2023-09-26 09:04:59 -0300

    Aulas 3: Operadores e Operações

---
+++ js/aula03.js
@@ -0,0 +1,32 @@
+// Operadores e Operações 
+
+let n1 = 10         // number inteiro
+let n2 = 2          // number inteiro
+let n3 = "2"        // string texto
+
+document.write("<h2>Operadores matemáticos</h2>") // Adição
+document.write(`<p> n1 + n2 = ${n1 + n2} <p>`) // Adição
+document.write(`<p> n1 - n2  = ${n1 - n2} <p>`) // Subtração
+document.write(`<p> n1 * n2  = ${n1 * n2} <p>`) // Multiplicação
+document.write(`<p> n1 / n2  = ${n1 / n2} <p>`) // Divisão
+document.write(`<p> resto de n1 / n2 = ${n1 % n2} <p>`) // Resto ou módulo
+
+document.write("<h2>Operadores de comparação</h2>") // Adição
+document.write(`<p> n1 é igual a n2? ${n1 == n2} <p>`) // Igual a
+document.write(`<p> n1 é identico a n2? ${n1 === n2} <p>`) // Identico a
+document.write(`<p> n1 é diferente de n2? ${n1 != n2} <p>`) // Diferente ou Não igual
+document.write(`<p> n1 é não é idenntico a n2? ${n1 !== n2} <p>`) // Não é identico
+
+document.write(`<p> n1 > n2? ${n1 > n2} <p>`) // maior que
+document.write(`<p> n1 < n2? ${n1 < n2} <p>`) // menor que
+document.write(`<p> n1 >=  n2 ${n1 >= n2} <p>`) // maior ou igual a
+document.write(`<p> n1 <=  n2${n1 >= n2} <p>`) // menor ou igual a
+
+document.write("<h2>Operadores lógicos</h2>") // menor ou igual a
+let a = true        // valor booleano
+let b = false       // valor booleano
+document.write(`<p> a and b = ${a && b} <p>`) // E ou AND lógico
+document.write(`<p> a or b = ${a || b} <p>`) // E ou AND lógico
+document.write(`Negação de a é = ${!a} e negação de b é = ${!b}`)     // Não ou NOT lógico
+
+
